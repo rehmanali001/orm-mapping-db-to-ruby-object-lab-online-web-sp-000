f58d92ad23e268cac6fa2888983a2ee5cf51b7a6
Rehman Ali <rehmanali001@gmail.com>
2019-09-04 01:04:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,11 @@
 class Student
   attr_accessor :id, :name, :grade
-  
-  def initialize(id, name, grade)
-    @name = name 
-    @id = id 
-    @grade = grade 
-  end
 
   def self.new_from_db(row)
-    new_student = self.new(row[0], row[1], row[2])
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
     new_student
   end
 
